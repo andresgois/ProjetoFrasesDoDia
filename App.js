4eb06d4f68dceb56f4383c81e056497d8a09f877
Andre Gois <andre.s.gois3@gmail.com>
2020-08-13 21:20:18 -0300

    inclusão do menu, criação do rodape e criado mais dois blocos centrais

--- App.js
+++ App.js
@@ -5,8 +5,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.menu}>
+        <Text style={styles.link}>Menu</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.tex}>Application</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.tex}>Application</Text>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.tex}>Application</Text>
+      </View>
+      <View style={styles.rodape}>
+        <Text style={styles.text}>Todos os direitos reservados</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +27,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#836FFF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-start",
   },
+  bloco:{
+    alignItems: 'flex-start',
+    width: 210,
+    height: 50,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+  },
+  tex:{
+    alignItems: 'center',
+    color: '#000',
+    fontSize: 20,
+  },
+  menu:{
+    alignItems: 'center',
+    width: 350,
+    height: 60,
+    backgroundColor: '#6A5ACD',
+    paddingTop: 10,
+    paddingBottom: 5,
+  },
+  link:{
+    fontSize: 50,
+    color: '#fff',
+    alignItems: 'center'
+  },
+  rodape:{
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    width: 350,
+    height: 60,
+  },
+  text:{
+    fontSize: 15,
+    color: '#fff'
+  }
 });
