e8acc8587b823e918a5c6f0fb23ebafc47c13897
Andre Gois <andre.s.gois3@gmail.com>
2020-08-15 12:58:53 -0300

    usando a propriedade shadow

--- App.js
+++ App.js
@@ -4,68 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.menu}>
-        <Text style={styles.link}>Menu</Text>
-      </View>
-      <View style={styles.bloco}>
-        <Text style={styles.tex}>Application</Text>
-      </View>
-      <View style={styles.bloco}>
-        <Text style={styles.tex}>Application</Text>
-      </View>
-      <View style={styles.bloco}>
-        <Text style={styles.tex}>Application</Text>
-      </View>
-      <View style={styles.rodape}>
-        <Text style={styles.text}>Todos os direitos reservados</Text>
-      </View>
+    <View>
+        <Text style={ Estilo.estiloText }>
+          Frases do Dia
+        </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#836FFF',
-    alignItems: 'center',
-    justifyContent: "flex-start",
-  },
-  bloco:{
-    alignItems: 'flex-start',
-    width: 210,
-    height: 50,
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 5,
-  },
-  tex:{
-    alignItems: 'center',
-    color: '#000',
-    fontSize: 20,
-  },
-  menu:{
-    alignItems: 'center',
-    width: 350,
-    height: 60,
-    backgroundColor: '#6A5ACD',
-    paddingTop: 10,
+const Estilo = StyleSheet.create({
+  estiloText:{
+    fontSize: 30,
+    backgroundColor: '#08509B',
+    shadowColor: '#000',
+    paddingTop: 5,
     paddingBottom: 5,
-  },
-  link:{
-    fontSize: 50,
-    color: '#fff',
-    alignItems: 'center'
-  },
-  rodape:{
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    width: 350,
-    height: 60,
-  },
-  text:{
-    fontSize: 15,
-    color: '#fff'
+    shadowOffset: {width:-5, height:25},
+    shadowOpacity: 0.4,
+    shadowRadius: 8,
   }
 });
