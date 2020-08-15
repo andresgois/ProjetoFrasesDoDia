331876a41c3f7cf09aff6b80dda8d8316dda7bc8
Andre Gois <andre.s.gois3@gmail.com>
2020-08-15 13:47:39 -0300

    Flexbox - utilizando flexDiretion, agora com dois componentes

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const { estiloText, estiloView } = Estilo;
+  const { estiloText, estiloText2, estiloView } = Estilo;
   return (
     <View style={estiloView}>
         <Text style={ estiloText }>
-          Frases do Dia
+          A
+        </Text>
+        <Text style={ estiloText2 }>
+          B
         </Text>
     </View>
   );
@@ -20,12 +23,20 @@ const Estilo = StyleSheet.create({
     height: 60,
     width: 60,
   },
+  estiloText2:{
+    fontSize: 40,
+    backgroundColor: '#2A48FA',
+    height: 60,
+    width: 60,
+  },
   estiloView: {
+    flex: 1,
     backgroundColor: 'skyblue',
     height: 300,
     // alinhamento na vertical
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
     // alinhamento na horizontal
     alignItems: 'center',
+    flexDirection: 'row',
   },
 });
