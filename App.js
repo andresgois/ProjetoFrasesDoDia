241d5b71635bd008520bdba259b0f5c347d2c49a
Andre Gois <andre.s.gois3@gmail.com>
2020-08-16 13:48:15 -0300

    utilizando componente Image e ImgeBackground

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
-const alerta = ()=>{
-  alert('Button pressionado!');
-}
 export default function App() {
-  const { principal, botao, textBoao } = Estilo;
+  const { principal,image } = Estilo;
   return (
     <View style={principal}>
-      <TouchableOpacity style={botao}>
-        <Text style={textBoao}>Clique aqui!</Text>
-      </TouchableOpacity>
+      {/* <Image style={image} source={ require('./imgs/uvas.png') }>
+        <Text>Legenda da foto</Text>
+      </Image> */}
+      {/* <Image source={{uri: 'http://localhost:19006/img/uvas.png'}} /> */}
+      <ImageBackground style={image} source={ require('./imgs/uvas.png')}>
+        <Text>Legenda da foto</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -19,23 +20,10 @@ export default function App() {
 const Estilo = StyleSheet.create({
   principal:{
     padding: 40,
-  },
-  botao:{
-    backgroundColor: '#48bbec',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 1,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 5, height: 5},
-    shadowOpacity: 0.5,
-    shadowRadius: 0.5,
-  },
-  textBoao:{
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    // centraliza o próprio objeto
-    alignSelf: 'center',
+  }, 
+  image:{
+    width:150,
+    height: 150,
+    justifyContent: 'center',
   }
 });
