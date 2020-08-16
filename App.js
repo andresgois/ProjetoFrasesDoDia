fe3785dd1a27842c474b26705d357ff2aa86de5f
Andre Gois <andre.s.gois3@gmail.com>
2020-08-16 11:55:38 -0300

    usando o TouchableOpacity para criação de button personalizado

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const alerta = ()=>{
   alert('Button pressionado!');
 }
 export default function App() {
-  const { principal } = Estilo;
+  const { principal, botao, textBoao } = Estilo;
   return (
     <View style={principal}>
-      <Button 
-        title = "Clique Aqui!"
-        color = "purple"
-        accessibilityLabel = "label de acessibilidade"
-        onPress = { alerta}
-      />
+      <TouchableOpacity style={botao}>
+        <Text style={textBoao}>Clique aqui!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -22,5 +19,23 @@ export default function App() {
 const Estilo = StyleSheet.create({
   principal:{
     padding: 40,
+  },
+  botao:{
+    backgroundColor: '#48bbec',
+    padding: 10,
+    borderColor: '#1d8eb8',
+    borderWidth: 1,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 5, height: 5},
+    shadowOpacity: 0.5,
+    shadowRadius: 0.5,
+  },
+  textBoao:{
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    // centraliza o próprio objeto
+    alignSelf: 'center',
   }
 });
