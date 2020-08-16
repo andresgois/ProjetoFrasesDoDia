cde8af3a4ffbcaf30534325083ee7e4b9ce36bdc
Andre Gois <andre.s.gois3@gmail.com>
2020-08-16 14:26:34 -0300

    finalizando app

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
 
 const botaoPress = () => {
-
+    var numAle = Math.floor(Math.random()* 5);
+    var frases = [
+      'O importante não é vencer todos os dias, mas lutar sempre.',
+      'Maior que a tristeza de não haver vencido é a vergonha de não ter lutado!',
+      'É melhor conquistar a si mesmo do que vencer mil batalhas.',
+      'Quem ousou conquistar e saiu pra lutar, chega mais longe!',
+      'Enquanto houver vontade de lutar haverá esperança de vencer.'
+    ];
+    Alert.alert(frases[numAle]);
 }
 export default function App() {
   const { principal, botao, textBot } = Estilo;
   return (
     <View style={principal}>
       <Image source={ require('./imgs/logo.png')} style={{width:253, height:140}} />
-      <TouchableOpacity style={botao}>
+      <TouchableOpacity 
+          onPress={botaoPress}
+          style={botao}>
         <Text style={textBot}>Nova frase</Text>
       </TouchableOpacity>
     </View>
