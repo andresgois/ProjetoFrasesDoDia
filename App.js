b0b91e5c90ebbaa32c36972de2db6bcb3550f2ac
Andre Gois <andre.s.gois3@gmail.com>
2020-08-16 11:44:12 -0300

    utilizando componetente button e sua propriedade onPress

--- App.js
+++ App.js
@@ -1,54 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const alerta = ()=>{
+  alert('Button pressionado!');
+}
 export default function App() {
-  const { principal, cont1, cont2, cont3, abaLatEsq, abaLatDir } = Estilo;
+  const { principal } = Estilo;
   return (
     <View style={principal}>
-      <View style={ cont1 }>
-        <Text>   Menu       </Text>
-      </View>
-      <View style={ cont2 }>
-        <View style={ abaLatEsq}>
-          <Text>abaLatEsq</Text>
-        </View>
-        <View style={ abaLatDir}>
-          <Text>abaLatDir</Text>
-        </View>
-      </View>
-      <View style={ cont3 }>
-        <Text>   Rodape       </Text>
-      </View>
+      <Button 
+        title = "Clique Aqui!"
+        color = "purple"
+        accessibilityLabel = "label de acessibilidade"
+        onPress = { alerta}
+      />
     </View>
   );
 }
 
 const Estilo = StyleSheet.create({
   principal:{
-    flex:1,
-    backgroundColor: 'cornflowerblue',
-  }, 
-  cont1:{
-    flex:2,
-    backgroundColor: 'brown',
-  }, 
-  cont2:{
-    flex:8,
-    backgroundColor: '#ccc',
-    flexDirection: "row",
-    justifyContent:"space-around",
-  }, 
-  cont3:{
-    flex:1,
-    backgroundColor: 'orangered',
-  },
-  abaLatEsq:{
-    width:150,
-    backgroundColor: '#fff',
-  },
-  abaLatDir:{
-    width:150,
-    backgroundColor: '#fff',
+    padding: 40,
   }
 });
